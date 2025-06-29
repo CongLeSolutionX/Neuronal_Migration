bfd09578841bdf0429c05e863b4a7c95f3108e0a
Cong Le <longchike@gmail.com>
2025-06-29 00:53:44 -0700

    The setup

---
+++ Neuronal_Migration/Neuronal_MigrationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Neuronal_MigrationApp.swift
+//  Neuronal_Migration
+//
+//  Created by Cong Le on 6/29/25.
+//
+
+import SwiftUI
+
+@main
+struct Neuronal_MigrationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
